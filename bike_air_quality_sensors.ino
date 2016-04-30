02d2f251e8a4367c8e52c1a4a914479154b81f9a
sighmon <simon.loffler@gmail.com>
2016-04-30 18:19:34 +0930

    Trying to work with an ARM compatible DHT22 library: https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTstable

--- bike_air_quality_sensors.ino
+++ bike_air_quality_sensors.ino
@@ -28,10 +28,10 @@ SYSTEM_MODE(MANUAL);
 
 #define DEVICE_NAME                "Air quality"
 
-#define CHARACTERISTIC1_MAX_LEN    15
-#define CHARACTERISTIC2_MAX_LEN    15
+#define CHARACTERISTIC1_MAX_LEN    20
+#define CHARACTERISTIC2_MAX_LEN    20
 
-#define TXRX_BUF_LEN               15
+#define TXRX_BUF_LEN               20
 
 /******************************************************
  *               Variable Definitions
@@ -176,12 +176,28 @@ Task coHeaterTask(60000, -1, &coHeaterTaskCallback);
 Scheduler runner;
 
 // DHT humidity/temp sensor
-#include "DHT.h"
-#define DHTPIN 2
-#define DHTTYPE DHT22
+//#include "DHT.h"
+//#define DHTPIN 2
+//#define DHTTYPE DHT22
 
 // Initialize DHT sensor.
-DHT dht(DHTPIN, DHTTYPE);
+//DHT dht(DHTPIN, DHTTYPE);
+
+#include <dht.h>
+dht DHT;
+#define DHT22_PIN 5
+
+struct
+{
+    uint32_t total;
+    uint32_t ok;
+    uint32_t crc_error;
+    uint32_t time_out;
+    uint32_t connect;
+    uint32_t ack_l;
+    uint32_t ack_h;
+    uint32_t unknown;
+} stat = { 0,0,0,0,0,0,0,0};
 
 
 // Sharp Optical Dust Sensor GP2Y10
@@ -216,8 +232,39 @@ float readCoSensor();
 void readSensorsTaskCallback() {
   struct SENSOR_READINGS readings;  
   // read temperature, humidity
-  readings.humidity = dht.readHumidity();
-  readings.temperature = dht.readTemperature();
+  uint32_t start = micros();
+  int chk = DHT.read22(DHT22_PIN);
+  uint32_t stop = micros();
+
+  stat.total++;
+  switch (chk)
+  {
+  case DHTLIB_OK:
+      stat.ok++;
+      Serial.print("OK,\t");
+      break;
+  case DHTLIB_ERROR_CHECKSUM:
+      stat.crc_error++;
+      Serial.print("Checksum error,\t");
+      break;
+  case DHTLIB_ERROR_TIMEOUT:
+      stat.time_out++;
+      Serial.print("Time out error,\t");
+      break;
+  default:
+      stat.unknown++;
+      Serial.print("Unknown error,\t");
+      break;
+  }
+  // DISPLAY DATA
+  Serial.print(DHT.humidity, 1);
+  Serial.print(",\t");
+  Serial.print(DHT.temperature, 1);
+  Serial.print(",\t");
+  Serial.print(stop - start);
+  Serial.println();
+//  readings.humidity = DHT.humidity;
+//  readings.temperature = DHT.temperature;
   // read particle sensor
   readings.particles = readDustSensor();
   // read CO sensor
@@ -347,7 +394,10 @@ void setup() {
   
   // DHT setup
   // Serial.println("=Setting up DHT22 temp/humidity sensor");
-  dht.begin();
+//  dht.begin();
+  Serial.println("dht22_test.ino");
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT_LIB_VERSION);
   
   // Sharp Optical Dust Sensor GP2Y10 setup
   pinMode(ledPower,OUTPUT);
@@ -358,5 +408,6 @@ void loop() {
   
   // Run tasks
   runner.execute();
+//  readSensorsTaskCallback();
 
 }
